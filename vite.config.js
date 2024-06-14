b6030c9a1805cab3286a81462c77bc58f27f766c
joker <jocker.ss.cat@gmail.com>
2024-06-14 18:14:57 +0800

    2

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: process.env.NODE_ENV === 'production' ? '/nani123/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/nani123-main/' : '/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
